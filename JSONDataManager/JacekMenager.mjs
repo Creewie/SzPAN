e7ba4ce3534feb5e5dd1f90824e08cad1c675770
Creewie <tabero2006@gmail.com>
2024-10-30 21:19:02 +0100

    is that a jojo reference?

--- JSONDataManager/JacekMenager.mjs
+++ JSONDataManager/JacekMenager.mjs
@@ -15,10 +15,8 @@ function askQuestion(question) {
     })
 }
 
-async function readJson(error = true)
+async function readJson(path, error = true)
 {
-    const path = await askQuestion("Podaj ścieżkę do pliku JSON: ");
-
     if (!fs.existsSync(path))
     {
         if (error) console.error("Ja rozumiem, że nie skończyłaś gegry rozszerzonej, ale chyba wiesz jakie pliki masz?");
@@ -30,29 +28,29 @@ async function readJson(error = true)
 async function main(){
     const czegoduszachce = (await askQuestion("Obiekt czy dane? ")).toLowerCase();
     try{
-        if (czegoduszachce == "dane"){            
-            console.log(await readJson());
+        if (czegoduszachce == "dane"){           
+            const path = await askQuestion("Podaj ścieżkę do pliku JSON: ");
+            console.log(await readJson(path));
         }
         else if(czegoduszachce == "obiekt"){
 
             const name = await askQuestion("Uprzejmie proszę o wpisanie swojego pełnego imienia w polu tekstowym, abyśmy mogli właściwie zidentyfikować Twoją tożsamość w celu dodanie go do tegoż obiektu: ");
             const age = await askQuestion("Wprowadź wiek obiektu który aktualnie będzie dopisywany do twojego pliku JSON(który może nie istnieć): ");
             const email = await askQuestion("Jeszcze proszę podać email tegoż obiektu, gdyż bez tego program sie wywali i się nie wykona, bo nie, bo tak uznaliśmy: ");
+            const path = await askQuestion("Podaj ścieżkę do pliku JSON: ");
 
-            const array = readJson(false);
+            const array = await readJson(path);
             const newJson = { name, age, email };
-            array.append(newJson);
-
-            // var json = JSON.stringify({name, age, email})
-            // fs.writeFile(path, json, {}, () => {})
-            
+            array.push(newJson);
+            const str = JSON.stringify(array);
+            fs.writeFile(path, str, {}, () => {});
         }
         else{
             console.error("Miałeś jedno zadanie łosiu")
         }
     }
     catch(err){
-        console.error("Masz downa!")
+        console.error(err)
     }
 }
 main();
