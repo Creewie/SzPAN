4eb4b3ee55f51109238ba23f36270741b03e27f2
Creewie <tabero2006@gmail.com>
2024-10-30 20:01:15 +0100

    Jednak podzial na foldery

---
+++ AsyncOperationsCalculator/KalkulatorPo40.mjs
@@ -0,0 +1,29 @@
+import fs from 'fs'
+import promises from "fs/promises"
+import readline from 'readline'
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function askQuestion(question) {
+    return new Promise(resolve=>{
+        rl.question(question,answer=>{
+            resolve(answer)
+        })
+    })
+}
+
+async function calc(){
+    try{
+        const liczba1 = await askQuestion("Podaj liczbe 1 ")
+        const liczba2 = await askQuestion("Podaj liczbe 2 ")
+        const znak = await askQuestion("Podaj operacje matematyczną")
+    }catch(err){
+        console.error(err)
+    }finally{
+        rl.close()
+    }
+}
+calc()
