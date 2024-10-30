3a636cf9773b6046e75a1faca4b5ddc3cb6aeaa7
Creewie <tabero2006@gmail.com>
2024-10-30 20:23:07 +0100

    Suicide

--- AsyncOperationsCalculator/KalkulatorPo40.mjs
+++ AsyncOperationsCalculator/KalkulatorPo40.mjs
@@ -18,14 +18,46 @@ function askQuestion(question) {
 async function calc(){
     try{
         const liczba1 = await askQuestion("Podaj liczbe 1 ")
+        const znak = await askQuestion("Podaj znak operacji matematycznej ")
         const liczba2 = await askQuestion("Podaj liczbe 2 ")
-        const znak = await askQuestion("Podaj operacje matematyczną")
-        const metoda = await askQuestion("Podaj metode przeprowadzania obliczeń(callback/promise)")
-        
+        const metoda = await askQuestion("Podaj metode przeprowadzania obliczeń (callback/promise) ")
+
+        if (isNaN(liczba1) || isNaN(liczba2))
+        {
+            throw new Error("Ty pajacu nie umiesz liczby napisać?");
+        }
+
+        switch (znak)
+        {
+            case "+":
+                break;
+            case "-":
+                break;
+            case "*":
+                break;
+            case "/":
+                break;
+            default:
+                throw new Error("Co ty za czary rzucasz?")
+        }
+
+        console.log(await oblicz(liczba1, liczba2, znak));
+
     }catch(err){
         console.error(err)
     }finally{
         rl.close()
     }
 }
+
+async function oblicz(liczba1, liczba2, znak)
+{
+    await sleep(2000);
+    return eval(`${liczba1}${znak}${liczba2}`);
+}
+
+const sleep = async(ms) => new Promise((resolve) => {
+    setTimeout(resolve, ms)
+});
+
 calc()
